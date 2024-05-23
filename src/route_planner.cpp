dc271c32e9f37402356bd98094f19f76a8f2d951
Richardvdketterij <richard.van.de.ketterij@nobleo.nl>
2024-05-23 09:49:11 +0200

    complete TODO 4

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -22,8 +22,7 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 // - Node objects have a distance method to determine the distance to another node.
 
 float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
-    float h = node->distance(*end_node);
-    return h;
+    return node->distance(*end_node);
 }
 
 
@@ -35,7 +34,15 @@ float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
 // - For each node in current_node.neighbors, add the neighbor to open_list and set the node's visited attribute to true.
 
 void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
+    current_node->FindNeighbors();
 
+    for (auto node: current_node->neighbors){
+        node->parent = current_node;
+        node->g_value = current_node->g_value + current_node->distance(*node);
+        node->h_value = CalculateHValue(node);
+        node->visited = true;
+        open_list.push_back(node);
+    }
 }
 
 
